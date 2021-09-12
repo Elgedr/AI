233333e0be56178c29bfedaa5e57cf04582fa5d3
elgedr <elgedr@ttu.ee>
2021-09-13 00:36:04 +0300

    commit 3

--- path_finding_on_a_2D_map/path_finding.py
+++ path_finding_on_a_2D_map/path_finding.py
@@ -1,4 +1,4 @@
-from queue import Queue
+from queue import Queue, PriorityQueue
 
 
 def bfs(given_map, start_coordinates):
@@ -70,23 +70,103 @@ def bfs(given_map, start_coordinates):
     print(res)
 
 
+def heuristic(goal, node):
+    return abs(goal[0] - node[0]) + abs(goal[1] - node[1])
+
+
+def greedy(given_map, start_coordinates, goal):
+    loop = 0
+    frontier = PriorityQueue()
+    frontier.put((0, start_coordinates))
+    came_from = {
+        start_coordinates: None}  # key: where we can go from current coordinate , value: our current coordinate
+    finish = None
+    path = []
+
+    while not frontier.empty():
+        loop += 1
+        current_first = frontier.get()
+        current = current_first[1]
+        neighbours = []
+        row = given_map[current[1]]
+        point = row[current[0]]
+        columns_amount = len(row)
+        rows_amount = len(given_map)
+        row_nr = current[1]
+        column_nr = current[0]
+
+        if point == "D":  # if current point is the diamond we break the loop
+            finish = current  # save diamond coordinates
+            break
+
+        elif point != "*":
+            # here I add up and down coordinates
+
+            if rows_amount == row_nr + 1:  # if we are on the last row, we cant check the row below us, so we add only
+                # coordinate above us
+                neighbours.append((column_nr, row_nr - 1))
+            elif 0 == row_nr:  # if we are on the first row, we cant check the row above us, so we add only coordinates below
+                neighbours.append((column_nr, row_nr + 1))
+            else:
+                neighbours.append((column_nr, row_nr - 1))  # otherwise we add both  - below and above coordinates
+                neighbours.append((column_nr, row_nr + 1))
+
+            # here I add left and right coordinates
+            if columns_amount == column_nr + 1:  # if we are on the last column, we cant check the next column, so we add only back column coordinates
+                neighbours.append((column_nr - 1, row_nr))
+            elif 0 == column_nr:  # if we are on the first column, we cant check the back column, so we add only next column coordinates
+                neighbours.append((column_nr + 1, row_nr))
+
+            else:
+                neighbours.append((column_nr - 1, row_nr))  # otherwise we add both  - left and right coordinates
+                neighbours.append((column_nr + 1, row_nr))
+
+        for neighbour_coordinate in neighbours:
+            if neighbour_coordinate not in came_from.keys():  # if we were not on this coordinate, we ...
+                priority = heuristic(goal, neighbour_coordinate)
+                frontier.put((priority, neighbour_coordinate))  # move frontier to other coordinate
+                came_from[
+                    neighbour_coordinate] = current  # add neighbour coordinate as possible movement from the current coordinate
+
+    where_i_am_now = finish  # saving diamond coordinates
+    path.append(
+        where_i_am_now)  # adding diamond coordinates as a start of the path (later I will reverse the path, so it will be the finish coordinate)
+    while start_coordinates not in path:
+        where_i_am_now = came_from.get(where_i_am_now)
+        path.append(where_i_am_now)
+    path.reverse()
+
+    res = []
+    for i in path:
+        a = i[1]
+        b = i[0]
+        res.append((a, b))
+    print(res)
+    print(loop)
+
+
 if __name__ == '__main__':
-    start = (16, 14)
-    lava_map1 = [
-        "     **********************    ",
-        "   *******   D    **********   ",
-        "   *******                     ",
-        " ****************    **********",
-        "***********          ********  ",
-        "            *******************",
-        " ********    ******************",
-        "********                   ****",
-        "*****       ************       ",
-        "***               *********    ",
-        "*      ******      ************",
-        "*****************       *******",
-        "***      ****            ***** ",
-        "                               ",
-        "                s              ",
-    ]
-    bfs(lava_map1, start)
+    # start = (16, 14)
+    # lava_map1 = [
+    #     "     **********************    ",
+    #     "   *******   D    **********   ",
+    #     "   *******                     ",
+    #     " ****************    **********",
+    #     "***********          ********  ",
+    #     "            *******************",
+    #     " ********    ******************",
+    #     "********                   ****",
+    #     "*****       ************       ",
+    #     "***               *********    ",
+    #     "*      ******      ************",
+    #     "*****************       *******",
+    #     "***      ****            ***** ",
+    #     "                               ",
+    #     "                s              ",
+    # ]
+    # bfs(lava_map1, start)
+    start = (2, 2)
+    with open("cave300x300") as f:
+        map_data3 = [line.strip() for line in f.readlines() if len(line) > 1]
+
+    greedy(map_data3, start, (257, 295))
